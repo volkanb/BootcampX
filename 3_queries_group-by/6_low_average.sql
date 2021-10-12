252887ecfe2c116453a75eeb0db24ea421040ac1
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-12 19:34:00 -0400

    Bug fix

--- 3_queries_group-by/6_low_average.sql
+++ 3_queries_group-by/6_low_average.sql
@@ -1,4 +1,4 @@
-qSELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
 FROM students
 JOIN assignment_submissions ON student_id = students.id
 JOIN assignments ON assignments.id = assignment_id
